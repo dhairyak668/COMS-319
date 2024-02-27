d654a23c47bbd3927ec45061a429834d0a9e09e2
dhairyak668 <dhairyak668@gmail.com>
2024-02-27 16:24:43 -0600

    activity8

---
+++ Activity_08/dhairyak_Activity08_MoviesFromJSON.js
@@ -0,0 +1,24 @@
+fetch("./dhairyak_Activity08_MoviesFromJSON.json")
+    .then(response => response.json())
+    .then(myMovies => loadMovies(myMovies));
+
+function loadMovies(myMovies) {
+    var mainContainer = document.getElementById("goodmovies");
+    console.log(mainContainer);
+
+    for(let i = 0; i<myMovies.movies.length;i++){
+        let movieTitle = myMovies.movies[i].title;
+        let movieYear = myMovies.movies[i].year;
+        let movieimg = myMovies.movies[i].url;
+        
+        let mytr = document.createElement("div");
+
+        mytr.innerHTML = `
+        <div>
+            <h3>${movieTitle}</h3>
+            <p>${movieYear}</p>
+            <img src ="${movieimg}" width="200px">
+        </div> `;
+        mainContainer.appendChild(mytr);
+    }
+}
