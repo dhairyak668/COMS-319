2a6551689bc389f3821e27987f37290ca1f44cda
dhairyak668 <dhairyak668@gmail.com>
2024-04-30 19:49:27 -0500

    activity16

---
+++ Activity16/frontend/src/index.js
@@ -0,0 +1,15 @@
+//    Author: Dhairya Kachalia
+//    ISU Netid : dhairyak
+//    Date :  April 27, 2024
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./appfrontend.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
